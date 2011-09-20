864912855b367e231afd65947b63da5974503272
Federico <giraud.federico@gmail.com>
2011-09-20 22:43:46 +0200

    Corretto il funzionamento del loggher.

--- src/connect/logs.clj
+++ src/connect/logs.clj
@@ -4,31 +4,28 @@
   (:import [java.util Calendar GregorianCalendar])
   (:require [noir.session :as session]))
 
+;db.createCollection("mycoll", {capped:true, size:10000000})
+
 (defn wrap-logging [handler]
   (fn [req]
-    (let [[out exec-time] (exec-time (handler req))
-          headers (:headers req)
-          cookies (:cookies req)]
+    (let [[out exec-time] (exec-time (handler req))]
       (insert! :logs
         {:date (java.util.Date.) :resp-time exec-time
          :method (:request-method req) :uri (:uri req)
-         :referer (get headers "referer")
-         :session (:value (get cookies "ring-session"))
-         :status (:status out) :out-type (get headers "Content-Type")
+         :referer (get-in req [:headers "referer"])
+         :session (:value (get-in req [:cookies "ring-session"]))
+         :status (:status out) :out-type (get-in out [:headers "Content-Type"])
          :username (session/get :username)})
       out)))
 
 (defn format-log-date [date]
   (.format (java.text.SimpleDateFormat. "yy-MM-dd HH:mm:ss") date))
 
-(GregorianCalendar. 2008 Calendar/APRIL 16)
-
-(defn print-log [logs]
-  (doseq [log logs]
-    (println
-      (format "%s (%4.1f) %6s  %4s  %s" (format-log-date (:date log))
-        (or (:resp-time log) 0) (or (:username log) "nil")
-        (:method log) (:uri log)))))
+(defn print-log [log]
+  (println
+    (format "%s (%4.1f) %6s  %4s  %s  %s" (format-log-date (:date log))
+      (or (:resp-time log) 0) (or (:username log) "nil")
+      (:method log) (:uri log) (:out-type log))))
 
 (defn get-time-ago [& {:keys [years months days hours minutes seconds]}]
   (let [d (GregorianCalendar.)]
@@ -41,9 +38,15 @@
     (.getTime d)))
 
 (defn logs-since [& keys]
-  (fetch :logs :where {:date {:$gt (apply get-time-ago keys)}}))
+  (dorun (map print-log
+           (fetch :logs :where {:date {:$gt (apply get-time-ago keys)}}))))
 
 (defn delete-logs! []
   (destroy! :logs {}))
 
+(defn print-logs [& [num]]
+  (let [tail (reverse (fetch :logs :sort {:$natural -1} :limit (or num 100)))]
+    (dorun (map print-log tail))))
+
+;(print-logs 200)
 ;(print-log (logs-since :hours 3))
